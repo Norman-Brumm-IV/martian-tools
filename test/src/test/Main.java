d29e26a2003a38a718cc30469a77676f02231306
Norman Brumm <88732234+Norman-Brumm-IV@users.noreply.github.com>
2021-10-19 18:48:47 -0500

    adding images

---
+++ test/src/test/Main.java
@@ -0,0 +1,18 @@
+package test;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String inputText = sc.nextLine();
+        wordCount(inputText);
+    }
+    
+    public static void wordCount(String input) {
+        // start here...
+    }
+}
